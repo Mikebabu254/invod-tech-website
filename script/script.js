d39a819660805d62bdc960485903a0a70dd5ec13
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-06-06 20:06:38 +0300

    Introduced hamburger menu

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
+
+
+// Menu function
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu');
+    const mobileNavBar = document.getElementById('menu');
+    
+    menuIcon.addEventListener('click', function() {
+        mobileNavBar.classList.toggle('show');
+    });
+});
+
 window.onscroll = function(){
     var navBar = document.getElementById('navBar');
     if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
